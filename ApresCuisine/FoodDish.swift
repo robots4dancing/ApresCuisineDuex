b38dfa8fcbf86b4e62f02cc0be26cece534ec7a6
Valerie Greer <vagreer10@Valeries-MacBook-Pro.local>
2017-02-27 23:41:51 -0500

    FoodDish class completed.

---
+++ ApresCuisine/FoodDish.swift
@@ -0,0 +1,31 @@
+//
+//  FoodDish.swift
+//  ApresCuisine
+//
+//  Created by Valerie Greer on 2/27/17.
+//  Copyright © 2017 Shane Empie. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class FoodDish: PFObject, PFSubclassing {
+    
+    @NSManaged var dishName     :String
+    @NSManaged var dateEaten    :Date?
+    @NSManaged var rating       :Int
+    @NSManaged var reviewText   :String
+    
+    convenience init(dishName: String, dateEaten: Date, rating: Int, reviewText: String) {
+        self.init()
+        self.dishName = dishName
+        self.dateEaten = dateEaten
+        self.rating = rating
+        self.reviewText = reviewText
+    }
+    
+    static func parseClassName() -> String {
+        return "FoodDish"
+    }
+    
+}
